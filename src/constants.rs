c4ec8a3d5621cbd5419636ce3d5f86ab1510329d
luke-landry <luke.landry468@gmail.com>
2026-02-14 20:02:52 -0500

    change default benchmark N values

--- src/constants.rs
+++ src/constants.rs
@@ -26,7 +26,7 @@ pub const DEFAULT_BENCHMARK_GRAVITY_METHODS: &str = "newton,newton-parallel";
 pub const DEFAULT_BENCHMARK_INTEGRATOR_METHODS: &str = "euler,velocity-verlet";
 
 /// Default n values for benchmarking when the benchmark flag is set
-pub const DEFAULT_BENCHMARK_N_VALUES: &str = "2,3,5,10,15,25,50,75,100,200,300";
+pub const DEFAULT_BENCHMARK_N_VALUES: &str = "2,3,5,10,15,25,50,75,100,150,200,250,300";
 
 pub const DEFAULT_BENCHMARK_NUM_RUNS: usize = 5;
 
